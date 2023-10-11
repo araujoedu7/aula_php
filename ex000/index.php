10422810a7b2f1e010fec755d062d2fc30ed656f
Eduardo Araújo <110600044+araujoedu7@users.noreply.github.com>
2023-10-11 16:57:05 -0300

    Add files via upload

---
+++ ex000/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>aula php</title>
+</head>
+
+<body>
+    <?php
+$curso = "informatica";
+
+$nome = "Eduardo";
+$sobrenome = "Araujo";
+
+echo "Olá! $nome, $sobrenome 👾. Você esta cursando $curso?";
+
+const MINHA_CONSTANTE = "edu11";
+    ?>
+</body>
+
+</html>
---
+++ ex001/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula de tipos primitivos PHP</title>
+</head>
+<body>
+    <?php 
+   $int = 12 ;
+
+    $boolean = false ;
+
+   $float = 12.3 ;
+
+   $string = " Eduardo ";
+    
+    echo $int ,  $string ,  $float;
+
+
+    
+    if( $boolean  == true ){
+        echo "verdadeiro";
+    } else {
+        echo "falso";
+    };
+    echo "$string";
+    echo '$string';
+     ?>
+
+</body>
+</html>
